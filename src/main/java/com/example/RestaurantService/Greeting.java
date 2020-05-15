d113ad77e31a12222de8f4ddd6b2e77afa15308c
AnirudhTiwari <tiwariisonline@gmail.com>
2020-05-15 16:48:01 +0530

    Initial HelloWorld commit

---
+++ src/main/java/com/example/RestaurantService/Greeting.java
@@ -0,0 +1,10 @@
+package com.example.RestaurantService;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class Greeting {
+    private final long id;
+    private final String content;
+}
